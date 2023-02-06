0311bd4fe97cbdef494256453add96c76d854a6e
LeonardoMartignoni <leonardo.martignonii@gmail.com>
2023-02-06 17:58:42 +0100

    Added team objects

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const teamMembers = [
+    {
+        name: 'Wayne',
+        surname: 'Barnett',
+        role: 'Founder & CEO',
+        imgpath: './img/wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        name: 'Angela',
+        surname: 'Caroll',
+        role: 'Chief Editor',
+        imgpath: './img/angela-caroll-chief-editor.jpg'
+    },
+    {
+        name: 'Walter',
+        surname: 'Gordon',
+        role: 'Office Manager',
+        imgpath: './img/walter-gordon-office-manager.jpg'
+    },
+    {
+        name: 'Angela',
+        surname: 'Lopez',
+        role: 'Social Media Manager',
+        imgpath: './img/angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name: 'Scott',
+        surname: 'Estrada',
+        role: 'Developer',
+        imgpath: './scott-estrada-developer.jpg'
+    },
+    {
+        name: 'Barbara',
+        surname: 'Ramos',
+        role: 'Graphic Designer',
+        imgpath: './barbara-ramos-graphic-designer.jpg'
+    }
+]
+
+console.log(teamMembers);
